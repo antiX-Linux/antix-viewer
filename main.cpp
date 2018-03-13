f85b4e841599a580051ee16eded88760e0dbf56d
antix-skidoo <antix-skidoo@users.noreply.github.com>
2018-03-13 06:40:45 +0000

    (WIP) main.cpp: create fn dropElevatedPrivileges()

--- main.cpp
+++ main.cpp
@@ -24,6 +24,30 @@
 #include <QMessageBox>
 #include "antixview.h"
 
+//  if all's well, this fn will return 1
+static int dropElevatedPrivileges()
+{
+    //   COULD check, and conditionally skip           if (getuid() == 0) || geteuid == 0)
+
+    //    initgroups()
+    // ref:  https://www.safaribooksonline.com/library/view/secure-programming-cookbook/0596003943/ch01s03.html#secureprgckbk-CHP-1-SECT-3.3
+
+    //   change guid + uid   ~~  nobody (uid 65534), nogroup (gid 65534), users (gid 100)
+    if (setgid(65534) != 0) return 1;  //  ("Failed to set nonroot GID") so apparently NOT root
+    if (setuid(65534) != 0) return 1;  //  ("Failed to set nonroot UID") so apparently NOT root
+    //                                                          or failed due to brittle/flawed expectation that nobody and nogroup exist
+
+    // On systems with defined _POSIX_SAVED_IDS in the unistd.h file, it should be 
+    // impossible to regain elevated privs after the setuid() call, above.  Test, try to regain elev priv:
+    if (setuid(0) != -1)  return 0;   // and the calling fn should EXIT/abort the program
+    //  COULD also confirm that setregid() fails
+    
+    // change cwd, for good measure (if unable to, treat as overall failure)
+    if (chdir("/tmp") != 0)   return 0;  // consider fprint()ing or logging the failure reason
+        
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -33,6 +57,11 @@ int main(int argc, char *argv[])
     QString url;
     QString title;
 
+    if(!dropElevatedPrivileges())
+    {
+        exit(1);  // log a reason, display a dialogbox?
+    }
+    
     if (argc == 1) {
         url = "http://duckduckgo.com";
         title = "antiX Viewer";
